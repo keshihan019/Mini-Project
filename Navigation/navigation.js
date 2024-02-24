3872e208cdc4f6b4c97ee99db05fcbe7eab93661
ackothalawala <avishkakothalawala99@gmail.com>
2024-02-24 16:55:17 +0530

    update nav, App

---
+++ Navigation/navigation.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react'; 
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import firebase from '../src/firebase/config.js';
+
+import LoginScreen from './../src/screens/LoginScreen/LoginScreen.js';
+import RegistrationScreen from './../src/screens/RegistrationScreen/RegistrationScreen.js';
+import HomeScreen from './../src/screens/HomeScreen/HomeScreen.js';
+import OptionsSelection from '../src/screens/OptionsSelection/OptionSelection.js';
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Stack = createStackNavigator();
+
+export default function navigation() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((firestoreDocument) => {
+            const userData = firestoreDocument.data();
+            setLoading(false);
+            setUser(userData);
+          })
+          .catch((error) => {
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+          <>
+          <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+            <Stack.Screen name="OptionsSelection" component={OptionsSelection} />
+
+          </>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Navigation/navigationStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    HomeScreen: {
+        flex: 1,
+        backgroundColor: "blue",
+    },
+    });
