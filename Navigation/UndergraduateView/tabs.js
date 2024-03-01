86b744af171cf7ab3b20e1869a372a9118330a71
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2024-03-01 08:45:18 +0530

    added icons to settings - notifications - profile tabs respectively

--- Navigation/UndergraduateView/tabs.js
+++ Navigation/UndergraduateView/tabs.js
@@ -8,11 +8,11 @@ import OptionsSelection from '../../src/screens/Authentication/OptionsSelection/
 import JobSelection from '../../src/screens/Authentication/Job selection/JobSelection';
 
 ///Icons imported for the bottom Navbar
-import SearchIcon from '../../src/assets/icons/search.png';
+import NotificationsIcon from '../../src/assets/icons/notification.png';
 import HomeIcon from '../../src/assets/icons/Home.png';
-import SettingsIcon from '../../src/assets/icons/search.png';
+import SettingsIcon from '../../src/assets/icons/setting.png';
 import JobsAppliedIcon from '../../src/assets/icons/jobsApplied.png';
-import ProfileIcon from '../../src/assets/icons/search.png';
+import ProfileIcon from '../../src/assets/icons/user.png';
 
 const Tab = createBottomTabNavigator();
 
@@ -71,9 +71,63 @@ const Tabs = () => {
                 )
             }}
                 />
-            <Tab.Screen name="Register" component={RegisterScreen} />
-            <Tab.Screen name="Options" component={OptionsSelection} />
-            <Tab.Screen name="Job" component={JobSelection} />
+            <Tab.Screen name="Register" component={RegisterScreen} options = {{
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        <Image 
+                            source={SettingsIcon}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#019F99' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#019F99' : '#748c94', fontSize: 12,textAlign:'center'}}>
+                            Settings
+                        </Text>
+                    </View>
+                )
+            }}
+                 />
+            <Tab.Screen name="Options" component={OptionsSelection} options = {{
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        <Image 
+                            source={NotificationsIcon}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#019F99' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#019F99' : '#748c94', fontSize: 12,textAlign:'center'}}>
+                            Notifications
+                        </Text>
+                    </View>
+                )
+            }}
+                />
+            <Tab.Screen name="Job" component={JobSelection} options = {{
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        <Image 
+                            source={ProfileIcon}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#019F99' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#019F99' : '#748c94', fontSize: 12,textAlign:'center'}}>
+                            Profile
+                        </Text>
+                    </View>
+                )
+            }}
+                />
         </Tab.Navigator>
     );
 }
