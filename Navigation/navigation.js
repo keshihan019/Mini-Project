06d99aa4c6b707ca54ce2bcfaaa183a15fe7e573
ackothalawala <avishkakothalawala99@gmail.com>
2024-03-02 12:07:54 +0530

    Fix navigation screen issue

--- Navigation/navigation.js
+++ Navigation/navigation.js
@@ -38,7 +38,7 @@ const MainStack = () => {
 
       {/* UndergraduateView */}
       {/* Settings Screens */}
-      {/* <Stack.Screen name="AllSettings" component={AllSettings} />
+      <Stack.Screen name="AllSettings" component={AllSettings} />
       <Stack.Screen name="AccSettings" component={AccSettings} options={{ title: 'Account Settings' }} />
       <Stack.Screen name="NotifSettings" component={NotifSettings} options={{ title: 'Notification Settings' }} />
       <Stack.Screen name="PrivSettings" component={PrivSettings} options={{ title: 'Privacy Settings' }} />
@@ -46,7 +46,7 @@ const MainStack = () => {
       <Stack.Screen name="TermsConditions" component={TermsConditions} options={{ title: 'Terms & Conditions' }} />
       <Stack.Screen name="DeveloperInfo" component={DeveloperInfo} options={{ title: 'Developer Information' }} />
       <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} options={{ title: 'Privacy Policy' }} />
-      <Stack.Screen name="SoftwareVersion" component={SoftwareVersion} options={{ title: 'Software Version' }} /> */}
+      <Stack.Screen name="SoftwareVersion" component={SoftwareVersion} options={{ title: 'Software Version' }} />
     </Stack.Navigator>
   );
 }
